7e14491196062bd7a0a9bcef02491cb09872ce7d
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-05-23 21:37:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,5 +7,13 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  # post '/food' do
+  #   # params.to_s
+  #   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  # end
+
+  post '/food' do
+  "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+end
 
 end
